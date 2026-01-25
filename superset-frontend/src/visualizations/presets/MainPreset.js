1ede14cc270742357696842312a8b778aed37dc2
Ilya8t <Stepchenko8@mail.ru>
2026-01-25 20:28:09 +0300

    custom AG Grid fix для первого запуска

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -81,6 +81,7 @@ import { PivotTableChartPlugin as PivotTableChartPluginV2 } from '@superset-ui/p
 import { HandlebarsChartPlugin } from '@superset-ui/plugin-chart-handlebars';
 import { FilterPlugins } from 'src/constants';
 import AgGridTableChartPlugin from '@superset-ui/plugin-chart-ag-grid-table';
+import AgGridTableChartPluginCustom from '@superset-ui/plugin-chart-ag-grid-table-custom';
 import TimeTableChartPlugin from '../TimeTable';
 
 export default class MainPreset extends Preset {
@@ -98,6 +99,14 @@ export default class MainPreset extends Preset {
     const agGridTablePlugin = isFeatureEnabled(FeatureFlag.AgGridTableEnabled)
       ? [
           new AgGridTableChartPlugin().configure({
+            key: VizType.TableAgGrid,
+          })
+        ]
+      : [];
+
+    const agGridTablePluginCustom = isFeatureEnabled(FeatureFlag.AgGridTableEnabled)
+      ? [
+          new AgGridTableChartPluginCustom().configure({
             key: VizType.TableAgGrid,
           })
         ]
@@ -197,6 +206,7 @@ export default class MainPreset extends Preset {
         }).configure({ key: VizType.Cartodiagram }),
         ...experimentalPlugins,
         ...agGridTablePlugin,
+        ...agGridTablePluginCustom,
       ],
     });
   }
