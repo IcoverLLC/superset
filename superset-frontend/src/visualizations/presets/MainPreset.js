f2a9362b3e9dc7f846605ff11e67076b15d800d8
Ilya_Stepchenko <136064364+Ilya8t@users.noreply.github.com>
2026-01-25 18:51:07 +0300

    Add custom AG Grid table plugin registration

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -81,6 +81,9 @@ import { PivotTableChartPlugin as PivotTableChartPluginV2 } from '@superset-ui/p
 import { HandlebarsChartPlugin } from '@superset-ui/plugin-chart-handlebars';
 import { FilterPlugins } from 'src/constants';
 import AgGridTableChartPlugin from '@superset-ui/plugin-chart-ag-grid-table';
+import AgGridTableChartPluginCustom, {
+  AG_GRID_TABLE_CUSTOM_PLUGIN_KEY,
+} from '@superset-ui/plugin-chart-ag-grid-table-custom';
 import TimeTableChartPlugin from '../TimeTable';
 
 export default class MainPreset extends Preset {
@@ -96,7 +99,14 @@ export default class MainPreset extends Preset {
       : [];
 
     const agGridTablePlugin = isFeatureEnabled(FeatureFlag.AgGridTableEnabled)
-      ? [new AgGridTableChartPlugin().configure({ key: VizType.TableAgGrid })]
+      ? [
+          new AgGridTableChartPlugin().configure({
+            key: VizType.TableAgGrid,
+          }),
+          new AgGridTableChartPluginCustom().configure({
+            key: AG_GRID_TABLE_CUSTOM_PLUGIN_KEY,
+          }),
+        ]
       : [];
 
     super({
