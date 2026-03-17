5a4fed058862a0aa2b093037d54b966127a20511
Ilya_Stepchenko <136064364+Ilya8t@users.noreply.github.com>
2026-03-17 10:17:50 +0300

    Revert "Codex-generated pull request"

--- superset-frontend/plugins/plugin-chart-ag-grid-table_custom/src/AgGridTable/index.tsx
+++ superset-frontend/plugins/plugin-chart-ag-grid-table_custom/src/AgGridTable/index.tsx
@@ -369,61 +369,43 @@ const AgGridDataTable: FunctionComponent<AgGridTableProps> = memo(
 
     return (
       <div style={containerStyles} ref={containerRef}>
-        <div className="top-controls-container">
-          {serverPagination && (
-            <Pagination
-              currentPage={serverPaginationData?.currentPage || 0}
-              pageSize={
-                hasServerPageLengthChanged
-                  ? serverPageLength
-                  : serverPaginationData?.pageSize || 10
-              }
-              totalRows={rowCount || 0}
-              pageSizeOptions={[10, 20, 50, 100, 200]}
-              onServerPaginationChange={onServerPaginationChange}
-              onServerPageSizeChange={onServerPageSizeChange}
-              sliceId={id}
-            />
+        <div className="dropdown-controls-container">
+          {renderTimeComparisonDropdown && (
+            <div className="time-comparison-dropdown">
+              {renderTimeComparisonDropdown()}
+            </div>
           )}
-
-          <div className="dropdown-controls-container">
-            {renderTimeComparisonDropdown && (
-              <div className="time-comparison-dropdown">
-                {renderTimeComparisonDropdown()}
-              </div>
-            )}
-            {includeSearch && (
-              <div className="search-container">
-                {serverPagination && (
-                  <div className="search-by-text-container">
-                    <span className="search-by-text"> Search by :</span>
-                    <SearchSelectDropdown
-                      onChange={onSearchColChange}
-                      searchOptions={searchOptions}
-                      value={serverPaginationData?.searchColumn || ''}
-                    />
-                  </div>
-                )}
-                <div className="input-wrapper">
-                  <div className="input-container">
-                    <SearchOutlined />
-                    <input
-                      ref={inputRef}
-                      value={
-                        serverPagination ? searchValue : quickFilterText || ''
-                      }
-                      type="text"
-                      id="filter-text-box"
-                      placeholder="Search"
-                      onInput={onFilterTextBoxChanged}
-                      onFocus={handleSearchFocus}
-                      onBlur={handleSearchBlur}
-                    />
-                  </div>
+          {includeSearch && (
+            <div className="search-container">
+              {serverPagination && (
+                <div className="search-by-text-container">
+                  <span className="search-by-text"> Search by :</span>
+                  <SearchSelectDropdown
+                    onChange={onSearchColChange}
+                    searchOptions={searchOptions}
+                    value={serverPaginationData?.searchColumn || ''}
+                  />
+                </div>
+              )}
+              <div className="input-wrapper">
+                <div className="input-container">
+                  <SearchOutlined />
+                  <input
+                    ref={inputRef}
+                    value={
+                      serverPagination ? searchValue : quickFilterText || ''
+                    }
+                    type="text"
+                    id="filter-text-box"
+                    placeholder="Search"
+                    onInput={onFilterTextBoxChanged}
+                    onFocus={handleSearchFocus}
+                    onBlur={handleSearchBlur}
+                  />
                 </div>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
 
         <ThemedAgGridReact
@@ -537,6 +519,21 @@ const AgGridDataTable: FunctionComponent<AgGridTableProps> = memo(
             isActiveFilterValue,
           }}
         />
+        {serverPagination && (
+          <Pagination
+            currentPage={serverPaginationData?.currentPage || 0}
+            pageSize={
+              hasServerPageLengthChanged
+                ? serverPageLength
+                : serverPaginationData?.pageSize || 10
+            }
+            totalRows={rowCount || 0}
+            pageSizeOptions={[10, 20, 50, 100, 200]}
+            onServerPaginationChange={onServerPaginationChange}
+            onServerPageSizeChange={onServerPageSizeChange}
+            sliceId={id}
+          />
+        )}
       </div>
     );
   },
