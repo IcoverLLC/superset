3bd1fb43dbc4bfaa58bf9d28f8cb46a9f1bbee2e
Ilya_Stepchenko <136064364+Ilya8t@users.noreply.github.com>
2026-03-19 12:41:53 +0300

    fix(ag-grid-table-custom): rename blank filter labels

--- superset-frontend/plugins/plugin-chart-ag-grid-table_custom/src/AgGridTable/index.tsx
+++ superset-frontend/plugins/plugin-chart-ag-grid-table_custom/src/AgGridTable/index.tsx
@@ -467,7 +467,7 @@ const AgGridDataTable: FunctionComponent<AgGridTableProps> = memo(
               // Set Filter
               selectAll: t('Select All'),
               searchOoo: t('Search...'),
-              blanks: t('Blanks'),
+              blanks: t('N/A'),
               // Filter operations
               filterOoo: t('Filter'),
               applyFilter: t('Apply Filter'),
@@ -482,6 +482,8 @@ const AgGridDataTable: FunctionComponent<AgGridTableProps> = memo(
               notContains: t('Not Contains'),
               startsWith: t('Starts With'),
               endsWith: t('Ends With'),
+              blank: t('N/A'),
+              notBlank: t('Not N/A'),
               // Logical conditions
               andCondition: t('AND'),
               orCondition: t('OR'),
